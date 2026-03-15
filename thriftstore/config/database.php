99bac4bcb8a513e3f5fdc590e1209445e54429d2
John Dave <johndavelausa@gmail.com>
2026-03-16 00:21:14 +0800

    Enable SSL for Azure MySQL connection

--- thriftstore/config/database.php
+++ thriftstore/config/database.php
@@ -60,7 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA') ?: (env('AZURE_MYSQL_HOST') ? true : null),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
             ]) : [],
         ],
 
