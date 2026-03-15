617cd44c0a48d6b5142669bc5f20d78f7b263158
John Dave <johndavelausa@gmail.com>
2026-03-16 02:55:59 +0800

    Fix Nginx config syntax in startup script

--- thriftstore/deployment/startup.sh
+++ thriftstore/deployment/startup.sh
@@ -41,7 +41,7 @@ server {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
 
-    location ~ \.php\$ {
+    location ~ \.php$ {
         include fastcgi_params;
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_index index.php;
