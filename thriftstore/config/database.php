7722ccdb852b31b5dfaacc21f9b9821b4fa0f730
John Dave <johndavelausa@gmail.com>
2026-03-16 00:35:07 +0800

    Fix SSL CA path for Azure

--- thriftstore/config/database.php
+++ thriftstore/config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA') ?: (env('AZURE_MYSQL_HOST') ? true : null),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA') ?: (env('AZURE_MYSQL_HOST') ? '/etc/ssl/certs/ca-certificates.crt' : null),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
             ]) : [],
         ],
