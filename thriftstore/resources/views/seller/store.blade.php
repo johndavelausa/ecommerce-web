f91fd471df5b57c99870eb493d8ec7b51fa390bd
John Dave <johndavelausa@gmail.com>
2026-03-27 16:17:07 +0800

    removed store profile and settings

--- thriftstore/resources/views/seller/store.blade.php
+++ thriftstore/resources/views/seller/store.blade.php
@@ -1,9 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Store & Profile Settings') }}
-        </h2>
-    </x-slot>
+ 
 
     <div class="flex min-h-screen">
         @include('layouts.seller-sidebar')
